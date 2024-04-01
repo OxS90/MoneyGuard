b1a6533ee551a912e59f7810e2472ce3b06a265e
CiprianR <133102658+Cipick23@users.noreply.github.com>
2024-04-01 15:07:58 +0300

    responsive dashbord

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,37 +5,53 @@ import css from './Navigation.module.css';
 import { selectIsLoggedIn } from '../../redux/authorisation/selectors';
 import { SlChart, SlHome } from 'react-icons/sl';
 import { Balance } from '../Balance/Balance';
+import { RiMoneyDollarBoxLine } from 'react-icons/ri';
 
 export const Navigation = () => {
   // const { user } = useAuth();
   // const { isLoggedIn } = useAuth();
 
   return (
-    <div className={css.sidebar}>
-      <nav className={css.columLinks}>
-        <NavLink to="/" exact="true" className={css.link}>
-          <h3>
-            <span className={css.iconMargin}>
-              <SlHome />
-            </span>
-            Home
-          </h3>
-        </NavLink>
-
-        {selectIsLoggedIn && (
-          <NavLink to="/statistics" exact="true" className={css.link}>
+    <div className={css.responsiveBox}>
+      <section>
+        <nav className={css.linksBox}>
+          <NavLink to="/" exact="true" className={css.link}>
             <h3>
               <span className={css.iconMargin}>
-                <SlChart />
+                <SlHome />
               </span>
-              Statistics
+              Home
             </h3>
           </NavLink>
-        )}
-      </nav>
-      <div>
+
+          {selectIsLoggedIn && (
+            <NavLink to="/statistics" exact="true" className={css.link}>
+              <h3>
+                <span className={css.iconMargin}>
+                  <SlChart />
+                </span>
+                Statistics
+              </h3>
+            </NavLink>
+          )}
+          {selectIsLoggedIn && (
+            <NavLink to="/" exact="true" className={css.linkCurrency}>
+              <h3>
+                <span className={css.iconMargin}>
+                  <RiMoneyDollarBoxLine />
+                </span>
+                Currency
+              </h3>
+            </NavLink>
+          )}
+        </nav>
+      </section>
+
+      <div className={css.linksBox}>
         <Balance />
       </div>
+
+      <div className={css.currencyBox}>{/* <Currency /> */}</div>
     </div>
   );
 };
