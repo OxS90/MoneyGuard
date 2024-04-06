9b04197a104aa48860adfca2c97f9c3f439f1684
CiprianR <133102658+Cipick23@users.noreply.github.com>
2024-04-06 09:50:07 +0300

    responsive and autocomplete
    styles at Balance.module.css, styles at DinamicSidebar.js, styles at Navigation.styled.js, styles at TransactionList.styled.jsx, error fix
     Navigation.jsand  error fix at RegisterForm.js

--- src/components/Dashboard/SideBar/SpecialDivStyled.js
+++ src/components/Dashboard/SideBar/SpecialDivStyled.js
@@ -6,6 +6,7 @@ export const SpecialDivStyled = styled.div`
   @media screen and (min-width: 768px) and (max-width: 1279px) {
     display: flex;
     flex-direction: column;
+    margin-left: 32px;
   }
   @media screen and (min-width: 1280px) {
   }
