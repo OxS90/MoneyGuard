ea20bede80c4fc790adf68158a12c42421663a36
OxS90 <oxana.slivinschi@gmail.com>
2024-04-08 22:23:41 +0300

    .

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -2,12 +2,14 @@ import { StyledSectionHome } from 'pages/HomePage/HomePage.styled';
 import { BtnAddTransaction } from '../../components/BtnAddTransaction/BtnAddTransaction';
 import TransactionList from 'components/TransactionList/TransactionList';
 import React from 'react';
+import BtnTeamModal from 'components/BtnTeamModal/BtnTeamModal';
 
 const HomePage = () => {
   return (
     <StyledSectionHome>
       <TransactionList />
       <BtnAddTransaction />
+      <BtnTeamModal />
     </StyledSectionHome>
   );
 };
