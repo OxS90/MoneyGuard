b04f501cb5959c08308d30054ab9c6b441c89726
OxS90 <oxana.slivinschi@gmail.com>
2024-04-09 16:44:18 +0300

    Revert "."
    This reverts commit e210b61fb48aa4140b6e483e6baeeacd31b7a20b.

--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -4,17 +4,16 @@ export const StyledSectionHome = styled.section`
   position: relative;
   display: flex;
   flex-direction: column;
-  height: calc(100vh - 250px);
 
   @media only screen and (min-width: 768px) {
     width: 715px;
-    height: calc(100vh - 550px);
+    height: calc(100vh - 70px);
   }
 
   @media only screen and (min-width: 1280px) {
     margin-top: 20px;
     padding-left: 69px;
     width: 785px;
-    height: calc(100vh - 100px);
+    height: calc(100vh - 70px);
   }
 `;
